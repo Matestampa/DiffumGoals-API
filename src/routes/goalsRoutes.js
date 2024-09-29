9caaa5a6a089260fb6d0ad0883b5a60f9f60531b
Matestampa <paredpiso5@gmail.com>
2024-09-29 02:15:42 -0300

    added validator

--- src/routes/goalsRoutes.js
+++ src/routes/goalsRoutes.js
@@ -3,10 +3,17 @@ const express = require("express");
 
 const router = express.Router();
 
+const multer = require("multer");
+
 const GoalsController = require("../controllers/goalsControllers.js");
 
+//----Config of multer middleware to upload imgs -------
+const storage = multer.memoryStorage();
 
-router.post("/new",GoalsController.newGoal);
+const upload = multer({storage: storage});
+//-----------------------------------------------------
+
+router.post("/new",upload.single("img"),GoalsController.newGoal);
 
 router.get("/",GoalsController.getGoals);
 
