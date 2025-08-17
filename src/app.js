63b153750cf6535ec93938fe176c2084050ddaa5
Matestampa <paredpiso5@gmail.com>
2025-08-16 23:41:00 -0300

    Added cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const App = express();
 
+const cors=require("cors");
+
 const cookieParser = require("cookie-parser");
 //---------------------- IMPORT MIDLEWARES ---------------------
 
@@ -11,6 +13,14 @@ const goalsRoutes = require("./routes/goalsRoutes.js");
 
 //-------------------- GENERAL EXPRESS CONFIG ---------------------
 
+App.use(cors(
+    {
+        credentials:true,
+        origin:(origin,callback)=>{
+            callback(null,true);
+        }
+    }
+));
 
 App.use(express.json());
 App.use(cookieParser());
