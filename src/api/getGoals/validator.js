b0a12fb30a59687b29b893c680fdab3ccde9f78e
Matestampa <paredpiso5@gmail.com>
2026-03-07 23:38:09 -0300

    Took out the endpoint for getting the original image. And now the original images comes included in the response of every goal

--- src/api/getGoals/validator.js
+++ src/api/getGoals/validator.js
@@ -10,10 +10,6 @@ const getGoals_ValSchema = Joi.object({
     order: Joi.number().valid(1, -1).default(DFLT_ORDER)
 });
 
-const getGoal_originalImage_ValSchema = Joi.object({
-    id: Joi.string().pattern(/^[0-9a-fA-F]{24}$/).required()
-});
-
 
 function validate_getGoals(query) {
     const { error, value } = getGoals_ValSchema.validate(query);
@@ -31,19 +27,4 @@ function validate_getGoals(query) {
         };
 }
 
-function validate_getGoal_originalImage(params) {
-    const { error, value } = getGoal_originalImage_ValSchema.validate(params);
-
-    if (error) {
-        return { error: DEFLT_API_ERRORS.BAD_REQ(error.message), paramData: null };
-    }
-
-    return {
-        error: null,
-        paramData: {
-            id: value.id
-        }
-    };
-}
-
-module.exports = { validate_getGoals, validate_getGoal_originalImage };
+module.exports = { validate_getGoals };
